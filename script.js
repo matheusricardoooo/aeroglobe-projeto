18894e3738b8db7d5bf462917a65e727b472e030
matheusricardooooo <matheusricardodev@gmail.com>
2025-10-17 19:52:48 -0300

    função para botão do menu feita

---
+++ script.js
@@ -0,0 +1,9 @@
+    function showSidebar() {
+        const sidebar = document.querySelector(".menu-mobile");
+        sidebar.style.display = "flex";
+}
+
+    function hideSidebar() {
+        const sidebar = document.querySelector(".menu-mobile");
+        sidebar.style.display = "none";
+}
